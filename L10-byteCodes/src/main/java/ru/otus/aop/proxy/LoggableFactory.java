f1d43376df4b31c2e1546deac3539276aee89323
Dmitry Belokurskyy <dbelokursky@gmail.com>
2021-08-15 00:34:05 +0500

    L10-byteCodes hw

--- L10-byteCodes/src/main/java/ru/otus/aop/proxy/LoggableFactory.java
+++ L10-byteCodes/src/main/java/ru/otus/aop/proxy/LoggableFactory.java
@@ -8,8 +8,8 @@ import java.lang.reflect.Proxy;
 public class LoggableFactory<T extends Loggable> {
 
     @SneakyThrows
-    public T getInstance(Class<T> proxy, Class<T> target) {
-        InvocationHandler handler = new LogInvocationHandler(target.getConstructor().newInstance());
+    public T getInstance(Class<T> proxy, T target) {
+        InvocationHandler handler = new LogInvocationHandler(target);
         return (T) Proxy.newProxyInstance(LoggableFactory.class.getClassLoader(), new Class<?>[]{proxy}, handler);
     }
 }
--- L10-byteCodes/src/main/java/ru/otus/aop/proxy/ProxyDemo.java
+++ L10-byteCodes/src/main/java/ru/otus/aop/proxy/ProxyDemo.java
@@ -2,7 +2,7 @@ package ru.otus.aop.proxy;
 
 public class ProxyDemo {
     public static void main(String[] args) {
-        Loggable loggable = new LoggableFactory().getInstance(Loggable.class, TargetClass.class);
+        Loggable loggable = new LoggableFactory().getInstance(Loggable.class, new TargetClass());
         loggable.doSomething(1);
         loggable.doSomething(3, 4);
         loggable.doSomething(5, 6, "7");
